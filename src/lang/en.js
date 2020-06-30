a3b666e014fb286de81914217d28e46a119c2c42
lilongyun <K1ngbas3>
2020-06-30 09:57:49 +0800

    代码错误

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,13 +1,13 @@
 export default {
   login: {
     title: 'login'
+  },
+  common: {
+    daifukuan: 'To Pay',
+    daipeisong: 'To Ship',
+    daitihuo: 'To Collection',
+    yitihuo: 'Collected',
+    shouhoufuwu: 'Returns',
+    wodezitidian: 'My Collection Point'
   }
-  common:{
-     daifukuan: 'To Pay'
-     daipeisong: 'To Ship'
-     daitihuo:  'To Collection'
-     yitihuo: 'Collected'
-     shouhoufuwu: 'Returns'
-     wodezitidian: 'My Collection Point'
-}
 }
