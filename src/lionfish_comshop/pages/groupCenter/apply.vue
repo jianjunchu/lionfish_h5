efd93014c3f3ad8a1c1f68086fec5b1951776124
zfl092005 <zfl092005@163.com>
2020-08-11 17:08:10 +0800

    申请团长与小程序一样用邮编

--- src/lionfish_comshop/pages/groupCenter/apply.vue
+++ src/lionfish_comshop/pages/groupCenter/apply.vue
@@ -9,21 +9,47 @@
             <div class="form-item">
               <label class="form-item-control">{{$t('host.xingming')}}</label>
               <!--<input @input="inputRealName" class="form-item-input" data-key="3" placeholder="{{$t(host.shuruxingming)}}" type="text"/>-->
-              <input v-model="head_name" class="form-item-input" placeholder="" type="text"/>
+              <input v-model="head_name" class="form-item-input" :placeholder="$t('host.shuruxingming')" type="text"/>
             </div>
             <!--<div class="['form-item', (errors[2]===2?'error':'')]">-->
             <div class="form-item">
               <label class="form-item-control">Mobile</label>
               <!--<input @input="inputMobile" class="form-item-input" data-key="2" :focus="focus_mobile" maxlength="11" placeholder="请输入手机号码" type="text" :value="mobile"/>-->
-              <input v-model="mobile_detail" class="form-item-input"  maxlength="11" placeholder="" type="text" />
+              <input v-model="mobile_detail" class="form-item-input"  maxlength="11" placeholder="请输入手机号码" type="text" />
             </div>
+
             <div class="form-item">
-              <div class="form-item">
+              <label class="form-item-control">邮编</label>
+              <!--<input bindinput="inputZipCode" class="form-item-input" focus="{{focus_zip_code}}" placeholder="请输入邮编" type="text" value="{{zip_code}}"></input>-->
+              <input v-model="zip_code" class="form-item-input"  placeholder="请输入邮编" type="text" />
+            </div>
+
+            <div class="form-item">
+              <label class="form-item-control">大牌</label>
+              <!--<input bindinput="inputBlkNo"   class="form-item-input" focus="{{focus_blk_no}}" placeholder="请输入大牌号" type="text" value="{{blk_no}}"></input>-->
+              <input v-model="blk_no"   class="form-item-input"  placeholder="请输入大牌号" type="text" />
+            </div>
+
+            <div class="form-item">
+              <label class="form-item-control">路名</label>
+              <!--<input bindinput="inputRoadName"   class="form-item-input" focus="{{focus_road_name}}" placeholder="请输入路名" type="text" value="{{road_name}}"></input>-->
+              <input v-model="road_name"   class="form-item-input"  placeholder="请输入路名" type="text" />
+            </div>
+
+            <div class="form-item">
+              <label class="form-item-control">门牌号</label>
+              <!--<input bindinput="inputLouMengHao"   class="form-item-input" focus="{{focus_lou_meng_hao}}" placeholder="请输入门牌号" type="text" value="{{lou_meng_hao}}"></input>-->
+              <input v-model="lou_meng_hao"   class="form-item-input"  placeholder="请输入门牌号" type="text" />
+            </div>
+
+            <!--<div class="form-item">-->
+              <!--<div class="form-item">-->
               <!-- <label class="form-item-control">微信号</label>-->
               <!--<input @input="inputWechat" class="form-item-input" :focus="focus_wechat" placeholder="请输入微信号" type="text" :value="wechat"/>-->
               <!-- <input v-model="wechat"  class="form-item-input" placeholder="" type="text" />-->
-              </div>
-            </div>
+              <!--</div>-->
+            <!--</div>-->
+
             <!--<div class="['form-item', (errors[5]===2?'error':'')]" style="display:none;">-->
               <!--<label class="form-item-control">验证码</label>-->
               <!--&lt;!&ndash;<input @input="inputCaptcha" class="form-item-input" data-key="5" maxlength="6" placeholder="请输入验证码" type="text"/>&ndash;&gt;-->
@@ -47,11 +73,11 @@
             <!--<img class="icon-right" src="@/assets/images/rightArrowImg.png"/>-->
             <!--</div>-->
             <!--<div class="['form-item', (errors[6]===2?'error':'')]">-->
-            <div class="form-item">
-              <label class="form-item-control">Address</label>
-              <!--<input @input="inputAddress" class="form-item-input" data-key="1" placeholder="请输入详细地址" type="text" :value="addr_detail"/>-->
-              <input v-model="addr_detail" class="form-item-input" placeholder="" type="text" />
-            </div>
+            <!--<div class="form-item">-->
+              <!--<label class="form-item-control">Address</label>-->
+              <!--&lt;!&ndash;<input @input="inputAddress" class="form-item-input" data-key="1" placeholder="请输入详细地址" type="text" :value="addr_detail"/>&ndash;&gt;-->
+              <!--<input v-model="addr_detail" class="form-item-input" placeholder="" type="text" />-->
+            <!--</div>-->
             <div class="form-item">
               <label class="submit">
                 Send
@@ -127,13 +153,59 @@
         token: '',
         community_id: '',
         is_need_subscript:false,
-        need_subscript_template:false
+        need_subscript_template:false,
+
+        lou_meng_hao:'',
+        road_name:'',
+        blk_no:'',
+        zip_code:'',
+        building:'',
+        lon:'',
+        lat:''
+      }
+    },
+    watch:{
+      zip_code: {
+        handler: function(zip_code) {
+          console.log(zip_code,'zip_code');
+
+          if("" != zip_code && /^\d{6}$/.test(zip_code)) {
+            this.load_address(zip_code);
+          }
+        },
+        immediate: true
       }
     },
     created: function() {
       this.onLoad();
     },
     methods: {
+      load_address:function(zipCode){
+        var tk = this.$wx.getStorageSync("token");
+        var that = this;
+
+        this.$http({
+            controller: "index.load_address",
+            token: tk,
+            zipCode: zipCode
+          }).then(e=> {
+            console.log(e);
+            var data = e;
+            console.log(data.results[0]);
+
+            if (data.found >0 ) {
+              var result =  data.results[0];
+
+              that.building = result.BUILDING;
+              that.blk_no = result.BLK_NO;
+              that.road_name = result.ROAD_NAME;
+              that.lon = result.LONGITUDE;
+              that.lat = result.LATITUDE;
+
+            }
+
+        });
+      },
       bindRegionChange: function(t) {
         this.region= t.detail.value.replace(/^\s*|\s*$/g, "");
       },
@@ -212,20 +284,65 @@
           var t = this, e = this.$wx.getStorageSync("token"), a = this.region[0], n = this.region[1], i = this.region[2],
             o = this.addr_detail, s = this.community_name, c = this.mobile_detail, u = this.lon_lat,
             r = this.head_name, l = this.wechat;
-          if ("" == r || void 0 === r) return this.$wx.showToast({
-            title: "Name is not correct",
-            icon: "none"
-          }), !1;
 
-          if ("" == c) return this.focus_mobile=!0, this.$wx.showToast({
-            title: "Mobile is not correct",
-            icon: "none"
-          }), !1;
+          if ("" == r || void 0 === r) {
+              this.$wx.showToast({
+              title: "Name is not correct",
+              icon: "none"
+            })
+            return false;
+          }
 
-          if (!/^\d{8}$/.test(c) && !/\d{8}/.test(c)) return this.focus_mobile= !0, this.$wx.showToast({
-           title: "Mobile is not correct",
-            icon: "none"
-          }), !1;
+          if ("" == c)  {
+            this.$wx.showToast({
+              title: "Mobile is not correct",
+              icon: "none"
+            });
+              return false;
+          }
+
+          if (!/^\d{8}$/.test(c) && !/\d{8}/.test(c))  {
+            this.$wx.showToast({
+              title: "Mobile is not correct",
+              icon: "none"
+            });
+            return false;
+          }
+
+          if ("" == this.zip_code) {
+            this.$wx.showToast({
+              title: "请输入邮编",
+              icon: "none"
+            });
+            return false;
+          }
+
+          if ("" == this.blk_no) {
+            this.$wx.showToast({
+              title: "请输入大牌号",
+              icon: "none"
+            });
+            return false;
+          }
+
+
+          if ("" == this.road_name) {
+            this.$wx.showToast({
+              title: "请输入路名",
+              icon: "none"
+            });
+            return false;
+          }
+
+          if ("" == this.lou_meng_hao) {
+            this.$wx.showToast({
+              title: "请输入单元门牌",
+              icon: "none"
+            });
+            return false;
+          }
+
+
 //          if ("" == l || void 0 === l) return this.$wx.showToast({
 //            title: "请填写微信号",
 //            icon: "none"
@@ -242,10 +359,10 @@
 //            title: "请选择地图位置",
 //            icon: "none"
 //          }), !1;
-          if ("" == o || void 0 === o) return this.$wx.showToast({
-            title: "address is not correct",
-            icon: "none"
-          }), !1;
+//          if ("" == o || void 0 === o) return this.$wx.showToast({
+//            title: "address is not correct",
+//            icon: "none"
+//          }), !1;
           var d = {
             province_name: a,
             city_name: n,
@@ -255,6 +372,12 @@
             community_name: s,
             mobile: c,
             head_name: r,
+            zip_code:this.zip_code,
+            blk_no:this.blk_no,
+            road_name:this.road_name,
+            lou_meng_hao:this.lou_meng_hao,
+            lon:this.lon,
+            lat:this.lat,
             wechat: l,
             controller: "community.sub_community_head",
             token: e,
@@ -274,18 +397,37 @@
         e.apply_complete= !0 ;
         e.needAuth= !0;
 //        this.$http_post({
-//           t}).then(t=> {
+//          ...t}).then(t=> {
 //               console.log(t);
 //            if(0 == t.code ){
 //              this.$wx.showToast({
 //                title: "提交成功，等待审核",
 //                icon: "none",
-//                duration: 2e3
+//                duration: 2000
 //              });
 //              e.apply_complete= !0 ;
 //               e.needAuth= !0;
 //            }
 //        });
+
+        this.$app.util.request({
+          url: "entry/wxapp/user",
+          data: t,
+          method: "post",
+          dataType: "json",
+          success: function(t) {
+            if(0 == t.code ){
+              e.$wx.showToast({
+                title: "提交成功，等待审核",
+                icon: "none"
+              });
+
+              e.apply_complete= !0 ;
+              e.needAuth= !0;
+            }
+          }
+        });
+
       },
       onLoad: function() {
         var a = this;
